18815cbf341111c1c7eac9ddc0d5329f392590aa
Shantanu Patil <shantanu.patil@iongroup.com>
2020-07-26 15:09:36 +0530

    #1 Added basic commit tool capability

---
+++ src/main/java/com/github/shan96/git_custom_commit/CommitDialog.java
@@ -0,0 +1,29 @@
+package com.github.shan96.git_custom_commit;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.DialogWrapper;
+import javax.swing.*;
+import org.jetbrains.annotations.Nullable;
+
+public class CommitDialog extends DialogWrapper {
+
+  private final CommitPanel panel;
+
+  CommitDialog(@Nullable Project project) {
+    super(project);
+    panel = new CommitPanel(project);
+    setTitle("Commit");
+    setOKButtonText("OK");
+    init();
+  }
+
+  @Nullable
+  @Override
+  protected JComponent createCenterPanel() {
+    return panel.getMainPanel();
+  }
+
+  CommitMessage getCommitMessage() {
+    return panel.getCommitMessage();
+  }
+}
---
+++ src/main/java/com/github/shan96/git_custom_commit/CommitPanel.java
@@ -0,0 +1,45 @@
+package com.github.shan96.git_custom_commit;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VfsUtil;
+import java.io.File;
+import javax.swing.*;
+
+public class CommitPanel {
+  private JPanel mainPanel;
+  private JComboBox changeType;
+  private JComboBox changeScope;
+  private JTextField shortDescription;
+  private JTextArea longDescription;
+  private JTextField closedIssues;
+  private JTextArea breakingChanges;
+
+  CommitPanel(Project project) {
+    for (ChangeType type : ChangeType.values()) {
+      changeType.addItem(type);
+    }
+    File workingDirectory = VfsUtil.virtualToIoFile(project.getBaseDir());
+    Command.Result result =
+        new Command(
+                workingDirectory,
+                "git log --all --format=%s | grep -Eo '^[a-z]+(\\(.*\\)):.*$' | sed 's/^.*(\\(.*\\)):.*$/\\1/' | sort -n | uniq")
+            .execute();
+    if (result.isSuccess()) {
+      result.getOutput().forEach(changeScope::addItem);
+    }
+  }
+
+  JPanel getMainPanel() {
+    return mainPanel;
+  }
+
+  CommitMessage getCommitMessage() {
+    return new CommitMessage(
+        (ChangeType) changeType.getSelectedItem(),
+        (String) changeScope.getSelectedItem(),
+        shortDescription.getText().trim(),
+        longDescription.getText().trim(),
+        closedIssues.getText().trim(),
+        breakingChanges.getText().trim());
+  }
+}
